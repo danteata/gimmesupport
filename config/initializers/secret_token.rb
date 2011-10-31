268f2e5550fd395095d7c2c5cc67bf415ac78fb8
danteata <dan.abakah@gmail.com>
2011-10-31 14:48:07 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GimmeSupport::Application.config.secret_token = 'b1f783903a8ee4095f6a53f7cfd330db8fb156f8d9d4329beea43729d8558585d70707b2beba742ed61a04dab21cb53a60c34a3cba78d4bc9985e21f370aaf74'
